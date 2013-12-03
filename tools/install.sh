4f409fe1c02beaf496ad47397288689ed2cdf1f5
Aleksandar Diklic <rastasheep3@gmail.com>
2013-12-03 23:05:05 +0100

    Added install script

---
+++ tools/install.sh
@@ -0,0 +1,38 @@
+if [ -d "~/.gobrew" ]
+then
+  echo "\033[0;33mYou already have gobrew installed.\033[0m You'll need to remove ~/.gobrew if you want to install"
+  exit
+fi
+
+echo "\033[0;34mCloning gobrew...\033[0m"
+
+hash git >/dev/null && /usr/bin/env git clone https://github.com/grobins2/gobrew.git ~/.gobrew || {
+  echo "You have to install git first"
+  exit
+}
+
+shell="$1"
+if [ -z "$shell" ]; then
+  shell="$(basename "$SHELL")"
+fi
+
+export_command='export PATH="$HOME/.gobrew/bin:$PATH"'
+eval_command='eval "$(gobrew init -)"'
+
+case "$shell" in
+  bash )
+    profile=bash_profile
+    ;;
+  zsh )
+    profile=zshrc
+    ;;
+  ksh )
+    profile=profile
+    ;;
+esac
+
+echo "${export_command}" >> "$HOME/.$profile"
+echo "${eval_command}" >> "$HOME/.$profile"
+
+echo "\n\n\033[0;32mGobrew is now installed.\033[0m"
+echo "Run 'gobrew' command for information"
