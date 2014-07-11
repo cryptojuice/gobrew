bf6b940ecfee3107f71e1aaeaefdc69e299b9269
Nick Robinson <grobins2@gmail.com>
2014-07-10 20:54:40 -0400

    fix merge issue in install file

--- tools/install.sh
+++ tools/install.sh
@@ -34,11 +34,7 @@ esac
 echo "${export_command}" >> "$HOME/.$profile"
 echo "${eval_command}" >> "$HOME/.$profile"
 
-<<<<<<< HEAD
-echo -e "\n\n\033[0;32mGobrew is now installed.\033[0m"
-=======
 source "$HOME/.$profile"
 
 echo "\n\n\033[0;32mGobrew is now installed.\033[0m"
->>>>>>> 89a58258341e99397987698292b47ab133c17d3d
 echo "Run 'gobrew' command for information."
